3fad2a5f9073de57b7c738189cdd96b626338b60
Johan Blom <johancblom@gmail.com>
2020-04-12 16:39:17 +0100

    Show Loading and Error States

--- src/components/RestaurantList.vue
+++ src/components/RestaurantList.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <v-progress-circular
+      v-if="loading"
+      indeterminate
+      color="primary"
+      data-testid="loading-indicator"
+    />
+    <v-alert
+      v-if="loadError"
+      type="error"
+      data-testid="loading-error"
+    >Restaurants could not be loaded.</v-alert>
     <v-list-item v-for="restaurant in restaurants" :key="restaurant.id" data-testid="restaurant">
       <v-list-item-content>
         <v-list-item-title>{{ restaurant.name }}</v-list-item-title>
@@ -19,6 +30,8 @@ export default {
     loadRestaurants: 'restaurants/load',
   }),
   computed: mapState({
+    loading: state => state.restaurants.loading,
+    loadError: state => state.restaurants.loadError,
     restaurants: state => state.restaurants.records,
   }),
 };
--- tests/unit/components/RestaurantList.spec.js
+++ tests/unit/components/RestaurantList.spec.js
@@ -3,6 +3,7 @@ import Vuetify from 'vuetify';
 import Vuex from 'vuex';
 import {mount, createLocalVue} from '@vue/test-utils';
 import RestaurantList from '@/components/RestaurantList';
+import {_} from 'core-js';
 
 const findByTestId = (wrapper, testId, index) =>
   wrapper.findAll(`[data-testid="${testId}"]`).at(index);
@@ -21,10 +22,10 @@ describe('RestaurantList', () => {
   let restaurantsModule;
   let wrapper;
 
-  beforeEach(() => {
+  const mountWithStore = (state = {records, loading: false}) => {
     restaurantsModule = {
       namespaced: true,
-      state: {records},
+      state,
       actions: {
         load: jest.fn().mockName('load'),
       },
@@ -35,14 +36,44 @@ describe('RestaurantList', () => {
       },
     });
     wrapper = mount(RestaurantList, {localVue, store});
-  });
+  };
 
   it('loads restaurants on mount', () => {
+    mountWithStore();
+
     expect(restaurantsModule.actions.load).toHaveBeenCalled();
   });
 
-  it('displays the restaurants', () => {
-    expect(findByTestId(wrapper, 'restaurant', 0).text()).toBe('Sushi Place');
-    expect(findByTestId(wrapper, 'restaurant', 1).text()).toBe('Pizza Place');
+  it('displays the loading indicator while loading', () => {
+    mountWithStore({loading: true});
+    expect(wrapper.contains('[data-testid="loading-indicator"]')).toBe(true);
+  });
+
+  describe('when loading succeeds', () => {
+    beforeEach(() => {
+      mountWithStore();
+    });
+    it('does not display the loading indicator while not loading', () => {
+      expect(wrapper.contains('[data-testid="loading-indicator"]')).toBe(false);
+    });
+
+    it('does not display the error message', () => {
+      expect(wrapper.contains('[data-testid="loading-error"]')).toBe(false);
+    });
+
+    it('displays the restaurants', () => {
+      expect(findByTestId(wrapper, 'restaurant', 0).text()).toBe('Sushi Place');
+      expect(findByTestId(wrapper, 'restaurant', 1).text()).toBe('Pizza Place');
+    });
+  });
+
+  describe('when loading fails', () => {
+    beforeEach(() => {
+      mountWithStore({loadError: true});
+    });
+
+    it('displays the error message', () => {
+      expect(wrapper.contains('[data-testid="loading-error"]')).toBe(true);
+    });
   });
 });
